2d834a98dd3887f064e251afed016951752232a1
Bala Kagitalapalli <147422387+Balakagitalapalli@users.noreply.github.com>
2023-12-05 22:54:54 +0530

    #include<stdio.h> int main() {     int n,i,factors=0;     scanf("%d",&n);     for (i=1;i<n;i++)     {         if(n%i==0)         factors+=i;     }     printf("%d",factors); }

--- _Sum_of_factors.c
+++ _Sum_of_factors.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,factors=0;
+    int n,i,factors=0;
     scanf("%d",&n);
-    for(i=1;i<n;i++)
+    for (i=1;i<n;i++)
     {
         if(n%i==0)
         factors+=i;
